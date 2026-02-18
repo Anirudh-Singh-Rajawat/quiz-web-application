b0bbf0cd9e23d7c6470f96cb95a65bce59990c78
Anirudh <anirudh18rajawat@gmail.com>
2026-02-19 00:06:52 +0530

    Updated CORS for production

--- backend/quizapp/quizapp/src/main/java/com/java/quizapp/controller/QuestionController.java
+++ backend/quizapp/quizapp/src/main/java/com/java/quizapp/controller/QuestionController.java
@@ -6,7 +6,7 @@ import com.java.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("question")
 public class QuestionController {
